97c1b7ee51b918f8b13fd34ac0fa23a158cee7b3
Ian Waites <ian.waites@gmail.com>
2017-07-18 17:54:44 -0700

    All To-dos are done including stretch goal

--- starter-code/server.js
+++ starter-code/server.js
@@ -16,8 +16,17 @@ const PORT = process.env.PORT || 3000;
 // DONE: Include all of the static resources as an argument to app.use()
 app.use(express.static('./public'));
 
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+// DONE: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('/new', function(req, res) {
+  console.log('made that request')
+  res.sendFile('new.html', {root: './public'});
+})
 
+
+// app.get('/new', function(req, res) {
+//   console.log('made that request');
+//   res.sendFile(path.join(__dirname, '../new', 'new.html'));
+// })
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
@@ -28,5 +37,5 @@ app.post('/articles', bodyParser, function(request, response) {
 })
 
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log(`The server is running on port: "${PORT}"`)// DONE: Log to the console a message that lets you know which port your server has started on
 });
